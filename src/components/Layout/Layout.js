d8f70df0c90fb6e871458f595a4b0082a712a3b6
Boinzemwinde Josias Yannick Kologo <bjykologo@sqli.com>
2019-04-26 16:03:01 +0100

    Build Controls

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Aux from '../../hoc/Auxiliary'
+import classes from './Layout.module.css'
+
+const Layout = (props) => (
+    <Aux>
+        <div>
+            Toolbar, SideDrawer, Backdrop
+        </div>
+        <main className={classes.content}>
+            {props.children}
+        </main>
+    </Aux>
+);
+
+export default Layout;
