6955482a60ac9dac78e4a98649590064b0409fc8
Boinzemwinde Josias Yannick Kologo <bjykologo@sqli.com>
2019-06-14 09:01:18 +0100

    Improvements

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,6 +7,7 @@ import axios from "../../../axios-orders";
 import Input from "../../../components/UI/Input/Input";
 import withErrorHandler from '../../../hoc/WithErrorHandler/WithErrorHandler';
 import { purchaseBurger } from "../../../store/actions";
+import {checkValidity} from "../../../shared/utility";
 
 class ContactData extends Component {
     state = {
@@ -111,19 +112,6 @@ class ContactData extends Component {
         };
         this.props.onOrderBurger(order, this.props.token);
     };
-    checkValidity = (value, rules) => {
-        let isValid = true;
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid;
-        }
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid;
-        }
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid;
-        }
-        return isValid;
-    };
     inputChangedHandler = (event, inputIdentifier) => {
         const updatedOrderForm = {
             ...this.state.orderForm
@@ -132,7 +120,7 @@ class ContactData extends Component {
             ...updatedOrderForm[inputIdentifier]
         };
         updatedFormElement.value = event.target.value;
-        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+        updatedFormElement.valid = checkValidity(updatedFormElement.value, updatedFormElement.validation);
         updatedFormElement.touched = true;
         updatedOrderForm[inputIdentifier] = updatedFormElement;
 
